be7f5f1a9c823ee5126149825bc0000243c2c2c7
Marcus Andersson <mban009@gmail.com>
2013-09-12 17:59:22 +0200

    Added caesar.py and encoder.py

---
+++ caesar.py
@@ -0,0 +1,28 @@
+'''
+Created on 12 sep 2013
+
+@author: Marcus
+'''
+
+class Caesar(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        """
+        Constructor
+        """
+        
+    def encrypt(self, key, plaindata):
+        """
+        Encrypt data using Caesar cipher
+        """
+        
+        
+    def decrypt(self, key, ciperdata):
+        """
+        Decrypt data encrypted using Caesar cipher
+        """
+        
