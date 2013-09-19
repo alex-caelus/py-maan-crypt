04a02df40dba321bfdc183ec10b5b9cab71e7504
Alexander Nilsson <alex.caelus@gmail.com>
2013-09-19 14:14:22 +0200

    Fixed #2. Mono-Alpha-Substitution-Chiper is now implemented.

--- pymaancrypt/monoalphasubstitution.py
+++ pymaancrypt/monoalphasubstitution.py
@@ -4,7 +4,13 @@ Created on 12 sep 2013
 
 @author: Alexander
 """
-import encoder
+
+import random
+
+try:
+    import encoder
+except ImportError:
+    import pymaancrypt.encoder
 
 class MonoAlphaSubstitution(object):
     """
@@ -18,42 +24,89 @@ class MonoAlphaSubstitution(object):
         """
         pass
         
-    def encrypt(self, key, plaindata):
+    def encrypt(self, keyobj, plaindata):
         """
         Encrypt data.
         Arguments:
         key: scrambled list of the alphabeth to be used as key
         plaindata: Instance of pymaancrypt.encoder.BaseEncoder
 
-        >>> e.encrypt(list("ENKROYCVDQBFMZSAILHPGTXUJ"), encoder.EncoderEN("Encrypt me!"))
-        'OZKLJAPMO'
+        >>> e.encrypt(e.makeKey(encoder.EncoderEN("ENKROYCVDQWBFMZSAILHPGTXUJ")), encoder.EncoderEN("Encrypt me!"))
+        'OMKIUSHFO'
+        
+        >>> e.encrypt(e.makeKey(encoder.EncoderSV("ENKROYCVDQBFMZSÖAIÅLWHPGÄTXUJ")), encoder.EncoderSV(u"Kryptera mig, åäö!"))
+        'BIÄÖLOIEMDCXUJ'
 
         """
         
         m = plaindata.getEncoded()
 
-        kIndex = 0
         encrypted = []
 
-        for c in m:
-            encrypted.append(key[ord(c)-ord('A')])
+        key = keyobj['key'] if 'key' in keyobj else keyobj
+
+        for p in m:
+            if p not in key.keys():
+                raise LookupError("char %c does not exist in key" % (p,))
+            encrypted.append(key[p])
 
         return ''.join(encrypted)
         
-    def decrypt(self, key, ciperdata):
+    def decrypt(self, keyobj, cipherdata):
         """
         Decrypt data.
-        """
-        pass
-    
-    def generateRandomKeyEN(self, seed):
-        """
-        Generates a 
-        """
-        pass
+        
+        Here follows some usage examples:
 
-    def generateRandomKeySV(self, seed):
-        pass
+        >>> key = e.makeKey(encoder.EncoderEN("ENKROYCVDQWBFMZSAILHPGTXUJ"))
+        >>> e.decrypt(key, 'OMKIUSHFO')
+        'ENCRYPTME'
+
+        >>> key = e.makeKey(encoder.EncoderSV("ENKROYCVDQBFMZSÖAIÅLWHPGÄTXUJ"))
+        >>> e.decrypt(key, 'BIÄÖLOIEMDCXUJ')
+        'KRYPTERAMIGÅÄÖ'
+        """
+        
+        return self.encrypt(keyobj['inverted'], keyobj['encoder'](cipherdata))
+
+    def makeKey(self, input):
+        """
+        takes a encoder.BaseEncoder derived object and converts it to a dictionary with original alphabet as index
+
+        Example:
+        >>> key = e.makeKey(encoder.EncoderSV("ENKROYCVDQBFMZSÖAIÅLWHPGÄTXUJ"))
+        >>> key['key'] == {'A': 'E', 'B': 'N', 'C': 'K', 'D': 'R', 'E': 'O', 'F': 'Y', 'G': 'C', 'H': 'V', 'I': 'D', 'J': 'Q', 'K': 'B', 'L': 'F', 'M': 'M', 'N': 'Z', 'O': 'S', 'P': 'Ö', 'Q': 'A', 'R': 'I', 'S': 'Å', 'T': 'L', 'U': 'W', 'V': 'H', 'W': 'P', 'X': 'G', 'Y': 'Ä', 'Z': 'T', 'Ä': 'U', 'Å': 'X', 'Ö': 'J'}
+        True
+        """
+        alphabet = list(input.getAlphabet())
+        alphabetLen = len(alphabet)
+        i = 0
+        keyobj = {'key': {}}
+        
+        for k in input.getEncoded():
+            keyobj['key'][alphabet[i]] = k
+            i += 1
+        
+        keyobj["inverted"] = {v:k for k, v in keyobj['key'].items()}
+        keyobj["encoder"] = input.__class__
+
+        kLen = len(keyobj['key'])
+        iLen = len(keyobj["inverted"] )
+
+        if kLen != alphabetLen or iLen != alphabetLen:
+            raise AssertionError("Input is not a valid key")
+
+        return keyobj
+            
+    def generateRandomKey(self, encClass):
+        """
+        Generates a random key form the alphabet of the specified encoder
+
+        >>> len(e.generateRandomKey(encoder.EncoderSV)['key'])
+        29
+        """
+        s = encClass.getAlphabet(None)
+        return self.makeKey(encClass(''.join(random.sample(s, len(s)))))
 
 
 def testmodule():
